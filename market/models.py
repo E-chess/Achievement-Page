e4f04a579669b23b5be07fb16205dc2a65ea1608
lodyczekoladowe <65414380+lodyczekoladowe@users.noreply.github.com>
2021-11-11 08:39:11 +0100

    Improved achievement.html and database programming.

--- market/models.py
+++ market/models.py
@@ -4,8 +4,7 @@ from market import db
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(length=30), nullable=False, unique=True)
-    value = db.Column(db.Integer(), nullable=False)
-    unit_of_value = db.Column(db.String())
+    value = db.Column(db.String(), nullable=False)
     school_name = db.Column(db.String(), nullable=False)
     category = db.Column(db.String(), nullable=False)
     description = db.Column(db.String())
